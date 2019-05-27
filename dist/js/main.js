902c9ce34fd0bae15541eca9cb619c5790d8893a
Kirill <kotyunin.k@mail.ru>
2019-05-27 21:50:14 +0600

    Fixed media queries

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,4 +6,29 @@ $(function() {
         speed: 1100,
         dots: true
     });
+
+    $('.mobile-menu-btn').click(function(){
+    	$(this).toggleClass('is-active');
+    	$('.header-nav').toggleClass('header-nav_active').stop().slideToggle();
+    })
+
+    $(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			$('.header-nav_active').removeClass('header-nav_active').slideUp();
+			$('.mobile-menu-btn').removeClass('is-active');
+		}
+	}).click(function() {
+		$('.header-nav_active').removeClass('header-nav_active').slideUp();
+		$('.mobile-menu-btn').removeClass('is-active');
+	});
+	$('.header').click(function(e) {
+		e.stopPropagation();
+	});
+
+	$(window).resize(function(){
+		if($(window).width() > 768) {
+			$('.header-nav').removeAttr('style');
+		}
+	})
+
 });
